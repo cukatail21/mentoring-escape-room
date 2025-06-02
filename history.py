6aa92937872270e7051e976be02e812254f68a9b
Almog Ben Chen <99953268+Almogbc@users.noreply.github.com>
2025-06-02 14:54:17 +0300

    ok ok *this* is the last commit

--- history.py
+++ history.py
@@ -4,17 +4,24 @@ import json
 class History:
     
     def __init__(self):
-        # TODO: make sure duplicate promts are not lost
+        # TODO: make sure duplicate prompts are not lost
         self.history_dict = {
             "bot":[],
             "users":[]
         }   
 
-    # doesnt work, here for nostalgic values
+    # does work, but not used
     def get_history(self):
         return self.history_dict
     
     def update_history(self,message, user, bot):
+        """
+        Updates the history with a new message.
+        PARAMS:
+            message: the message to add
+            user: the user who sent the message
+            bot: true if the message is from the bot, false if it is from a user
+        """
         to_add = {user: message}
         if bot:
             self.history_dict["bot"].append(to_add)
@@ -22,6 +29,7 @@ class History:
         self.history_dict["users"].append(to_add)
 
     def reset_history(self):
+        """Resets the history to the initial state."""
         self.history_dict = {
             "bot":[],
             "users":[]
